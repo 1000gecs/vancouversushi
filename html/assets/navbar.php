65902ea522005f6e5bff93c59730e34d96568def
BuildTools <unconfigured@null.spigotmc.org>
2022-08-15 16:28:07 -0400

    Moved navbar to singular file and refactored accordingly

---
+++ html/assets/navbar.php
@@ -0,0 +1,7 @@
+<div id="websiteName">
+<a href="/">Vancouver Sushi Tier-list</a>
+</div>
+<nav class="navBar">
+    <a href="/vote">Vote</a>
+    <a href="/community/">Community</a>
+</nav>
---
+++ html/vote/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="../assets/style.css">
+    <title>Vancouver Sushi</title>
+</head>
+<header>
+    <?php include_once("../assets/navbar.php"); ?>
+</header>
+<body>
+
+</body>
+</html> 
